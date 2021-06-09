a88429f49c1b1819ee954b07df0e74015c731400
benthebear93 <nswve@naver.com>
2021-06-09 17:01:29 +0900

    extract boundary

--- test_bed/include/preprocess_pointcloud.h
+++ test_bed/include/preprocess_pointcloud.h
@@ -4,6 +4,8 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include "std_msgs/Int32MultiArray.h"
+
 #include <pcl/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -11,11 +13,28 @@
 #include <pcl/io/pcd_io.h>
 #include <iostream>
 
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
 #include <typeinfo> 
 #include <pcl/filters/passthrough.h>
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/filters/filter_indices.h> // for pcl::removeNaNFromPointCloud
 
+#include <pcl/segmentation/region_growing_rgb.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+
+#include <pcl/filters/extract_indices.h>
+
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/point_cloud_color_handlers.h>
+#include <pcl/range_image/range_image.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/range_image_visualizer.h>
+#include <pcl/common/common_headers.h>
+#include <pcl/visualization/point_cloud_color_handlers.h>
+#include <pcl/features/range_image_border_extractor.h>
+#include <pcl/console/parse.h>
+#include <pcl/common/file_io.h> // for getFilenameWithoutExtension
 class PCprocess
 {
 public: 
@@ -24,12 +43,16 @@ public:
 	void cloud_cd(const sensor_msgs::PointCloud2 msg);
 	// void do_passthrough(const pcl::PointCloud<pcl::PointXYZRGB>& src, pcl::PointCloud<pcl::PointXYZRGB>& dst);
 	void read_pcd();
+	void segmentation(pcl::PointCloud<pcl::PointXYZRGB>::Ptr in_cloud);
+	void extract_border(pcl::PointCloud<pcl::PointXYZRGB>::Ptr in_cloud);
 private:
 	int argc;
 	char** argv;
 	ros::NodeHandle n_;
 	ros::Subscriber sub_;
 	ros::Publisher pub_;
+	ros::Publisher boundary_pub_x_;
+	ros::Publisher boundary_pub_y_;
 	bool flag_save;
 
 };
--- test_bed/src/preprocess_pointcloud.cpp
+++ test_bed/src/preprocess_pointcloud.cpp
@@ -8,12 +8,15 @@ PCprocess::PCprocess()
 {
   flag_save = 0;
   std::cout << "point cloud process start" << std::endl;
-  sub_ = n_.subscribe("/camera/depth/color/points", 1 , &PCprocess::cloud_cd, this);
-  //   "/camera/depth_registered/points"
+  sub_ = n_.subscribe("/pcl_output", 1 , &PCprocess::cloud_cd, this);
+  boundary_pub_x_ = n_.advertise<std_msgs::Int32MultiArray>("boundary_x", 100);
+  boundary_pub_y_ = n_.advertise<std_msgs::Int32MultiArray>("boundary_y", 100);
 }
+
 PCprocess::~PCprocess(){
 
 }
+// basic class 
 
 void PCprocess::read_pcd(){
   std::cout<<"test read" <<std::endl;
@@ -27,28 +30,156 @@ void PCprocess::read_pcd(){
             << cloud->width * cloud->height
             << " data points from test_file.pcd with the following fields: "
             << std::endl;
-  // for (const auto& point: *cloud)
-  //   std::cout << "    " << point.x
-  //             << " "    << point.y
-  //             << " "    << point.z << std::endl;
   }
 }
+// upload pcd
+
+void PCprocess::segmentation(pcl::PointCloud<pcl::PointXYZRGB>::Ptr in_cloud){
+  pcl::search::Search <pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
+  pcl::IndicesPtr indices (new std::vector <int>);
+  pcl::removeNaNFromPointCloud (*in_cloud, *indices);
+  pcl::RegionGrowingRGB<pcl::PointXYZRGB> reg;
+  reg.setInputCloud (in_cloud);
+  reg.setIndices (indices);
+  reg.setSearchMethod (tree);
+  reg.setDistanceThreshold (10);
+  reg.setPointColorThreshold (6);
+  reg.setRegionColorThreshold (5);
+  reg.setMinClusterSize (100);
+
+  std::vector <pcl::PointIndices> clusters;
+  reg.extract (clusters);
+
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cluster_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+
+
+  std::cout << "Number of clusters is equal to " << clusters.size () << std::endl;
+  std::cout << "First cluster has " << clusters[0].indices.size () << " points." << std::endl;
+  std::cout << "These are the indices of the points of the initial" <<
+  std::endl << "cloud that belong to the first cluster:" << std::endl;
+ int currentClusterNum = 1;
+  for (std::vector<pcl::PointIndices>::const_iterator i = clusters.begin(); i != clusters.end(); ++i)
+  {
+      // ...add all its points to a new cloud...
+      pcl::PointCloud<pcl::PointXYZRGB>::Ptr cluster(new pcl::PointCloud<pcl::PointXYZRGB>);
+      for (std::vector<int>::const_iterator point = i->indices.begin(); point != i->indices.end(); point++)
+          cluster->points.push_back(in_cloud->points[*point]);
+      cluster->width = cluster->points.size();
+      cluster->height = 1;
+      cluster->is_dense = true;
+
+      // ...and save it to disk.
+      if (cluster->points.size() <= 0)
+          break;
+      std::cout << "Cluster " << currentClusterNum << " has " << cluster->points.size() << " points." << std::endl;
+      std::string fileName = "/home/benlee/catkin_ws/src/Direct_machining_with_manipulator/test_bed/pcd_data/cluster" + boost::to_string(currentClusterNum) + ".pcd";
+      pcl::io::savePCDFileASCII(fileName, *cluster);
+
+      currentClusterNum++;
+  }
+}
+//segmentation
+
+void PCprocess::extract_border(pcl::PointCloud<pcl::PointXYZRGB>::Ptr in_cloud)
+{
+  typedef pcl::PointXYZ PointType;
+  float angular_resolution= 0.5f;
+  pcl::RangeImage::CoordinateFrame coordinate_frame = pcl::RangeImage::CAMERA_FRAME;
+  bool setUnseenToMaxRange = false;
+
+  setUnseenToMaxRange = true;
+  int tmp_coordinate_frame;
+  angular_resolution = pcl::deg2rad (angular_resolution);
+
+  pcl::PointCloud<PointType>::Ptr point_cloud_ptr (new pcl::PointCloud<PointType>); //making point cloUd
+  pcl::PointCloud<PointType>& point_cloud = *point_cloud_ptr; //getting address
+  pcl::PointCloud<pcl::PointWithViewpoint> far_ranges; //set range
+  Eigen::Affine3f scene_sensor_pose (Eigen::Affine3f::Identity ()); //set sensor pose
+  std::string filename = "/home/benlee/catkin_ws/src/Direct_machining_with_manipulator/test_bed/pcd_data/cluster10.pcd";
+  pcl::io::loadPCDFile (filename, point_cloud);
+  scene_sensor_pose = Eigen::Affine3f (Eigen::Translation3f (point_cloud.sensor_origin_[0],
+                                                             point_cloud.sensor_origin_[1],
+                                                             point_cloud.sensor_origin_[2])) *
+                      Eigen::Affine3f (point_cloud.sensor_orientation_);
+  std::string far_ranges_filename = pcl::getFilenameWithoutExtension (filename)+"_far_ranges.pcd";
+  if (pcl::io::loadPCDFile(far_ranges_filename.c_str(), far_ranges) == -1)
+      std::cout << "Far ranges file \""<<far_ranges_filename<<"\" does not exists.\n";
+   
+  float noise_level = 0.0;
+  float min_range = 0.0f;
+  int border_size = 1;
+  pcl::RangeImage::Ptr range_image_ptr (new pcl::RangeImage);
+  pcl::RangeImage& range_image = *range_image_ptr;   
+  range_image.createFromPointCloud (point_cloud, angular_resolution, pcl::deg2rad (360.0f), pcl::deg2rad (180.0f),
+                                   scene_sensor_pose, coordinate_frame, noise_level, min_range, border_size);
+  range_image.integrateFarRanges (far_ranges);
+  if (setUnseenToMaxRange)
+    range_image.setUnseenToMaxRange ();
+  
+  // -------------------------
+  // -----Extract borders-----
+  // -------------------------
+  pcl::RangeImageBorderExtractor border_extractor (&range_image);
+  pcl::PointCloud<pcl::BorderDescription> border_descriptions;
+  border_extractor.compute (border_descriptions);
+  std::cout << border_descriptions <<std::endl;
+  
+  // ----------------------------------
+  // -----Show points in 3D viewer-----
+  // ----------------------------------
+  pcl::PointCloud<pcl::PointWithRange>::Ptr border_points_ptr(new pcl::PointCloud<pcl::PointWithRange>),
+                                            veil_points_ptr(new pcl::PointCloud<pcl::PointWithRange>),
+                                            shadow_points_ptr(new pcl::PointCloud<pcl::PointWithRange>);
+  pcl::PointCloud<pcl::PointWithRange>& border_points = *border_points_ptr,
+                                      & veil_points = * veil_points_ptr,
+                                      & shadow_points = *shadow_points_ptr;
+  for (int y=0; y< (int)range_image.height; ++y)
+  {
+    for (int x=0; x< (int)range_image.width; ++x)
+    {
+      if (border_descriptions[y*range_image.width + x].traits[pcl::BORDER_TRAIT__OBSTACLE_BORDER])
+        border_points.points.push_back (range_image[y*range_image.width + x]);
+      if (border_descriptions[y*range_image.width + x].traits[pcl::BORDER_TRAIT__VEIL_POINT])
+        veil_points.points.push_back (range_image[y*range_image.width + x]);
+      if (border_descriptions[y*range_image.width + x].traits[pcl::BORDER_TRAIT__SHADOW_BORDER])
+        shadow_points.points.push_back (range_image[y*range_image.width + x]);
+    }
+  }
+
+  std_msgs::Int32MultiArray x_array;
+  std_msgs::Int32MultiArray y_array;
+  x_array.data.clear();
+  y_array.data.clear();
+  for (int i =0; i<border_points.points.size(); i++){
+    std::cout << "x: "<<300+int(border_points.points[i].x * 1000)<< std::endl;
+    int x = 300+int(border_points.points[i].x * 1000);
+    std::cout << "y: "<<int(border_points.points[i].y * 1000)<< std::endl;
+    int y = int(border_points.points[i].y * 1000);
+    x_array.data.push_back(x);
+    y_array.data.push_back(y);
+  }
+  boundary_pub_x_.publish(x_array);
+  boundary_pub_y_.publish(y_array);
+}
+
+
+
+
 
 void PCprocess::cloud_cd(const sensor_msgs::PointCloud2 msg){
     pcl::PCLPointCloud2 pcl_pc; //pcl point cloud
     pcl_conversions::toPCL(msg, pcl_pc); // sensor msg to pcl
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);//pointcloud2
     pcl::fromPCLPointCloud2(pcl_pc,*temp_cloud); //pcl to pointcloud2
-    // sensor_msgs::PointCloud2 ros_output;
-    // pcl::toPCLPointCloud2(*output_ptr, pcl_pc);
-    // pcl_conversions::fromPCL(pcl_pc, ros_output);
+
     if(flag_save ==0){
       std::cout << "saveing.."<< std::endl;
-      pcl::io::savePCDFile ("/home/benlee/catkin_ws/src/Direct_machining_with_manipulator/test_bed/pcd_data/real_pc.pcd", *temp_cloud);
+      pcl::io::savePCDFile ("/home/benlee/catkin_ws/src/Direct_machining_with_manipulator/test_bed/pcd_data/pcl_output.pcd", *temp_cloud);
       flag_save = 1;
       std::cout << "saved" << std::endl;
     }
-    //do_passthrough(*temp_cloud, ptfilter);
+
     pcl::PointCloud<pcl::PointXYZRGB> pc_filtered;
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr ptr_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::PassThrough<pcl::PointXYZRGB> ptfilter;
@@ -57,21 +188,26 @@ void PCprocess::cloud_cd(const sensor_msgs::PointCloud2 msg){
 
     ptfilter.setInputCloud(ptr_filtered);
     ptfilter.setFilterFieldName("z"); 
-    ptfilter.setFilterLimits(0.2, 0.313); //min. max
+    ptfilter.setFilterLimits(0.53, 0.600); //min. max
     ptfilter.setFilterLimitsNegative(false); //option 
     ptfilter.filter(*ptr_filtered);
+
     pc_filtered = *ptr_filtered;
     std::cout << "filtered saving..." << std::endl;
-    pcl::io::savePCDFile ("/home/benlee/catkin_ws/src/Direct_machining_with_manipulator/test_bed/pcd_data/pass_real_pc.pcd", pc_filtered);
+    pcl::io::savePCDFile ("/home/benlee/catkin_ws/src/Direct_machining_with_manipulator/test_bed/pcd_data/pass_pcl_output.pcd", pc_filtered);
     std::cout << "filtered saved" << std::endl;
 
+    PCprocess::segmentation(ptr_filtered);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr segmented_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::io::loadPCDFile<pcl::PointXYZRGB> ("/home/benlee/catkin_ws/src/Direct_machining_with_manipulator/test_bed/pcd_data/cluster10.pcd", *segmented_pc);
+    PCprocess::extract_border(segmented_pc);
 }
 
 int main(int argc, char **argv){
   std::cout << "start!"<< std::endl;
   ros::init (argc, argv, "pc_process");
   PCprocess wp_object;
-  //wp_object.read_pcd();
+  wp_object.read_pcd();
   ros::spin ();
   return 0;
 }
