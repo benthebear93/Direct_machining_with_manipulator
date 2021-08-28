ffff8bbf4f440f0fe05699c56e759d78e3e8e09c
benthebear93 <nswve@naver.com>
2021-08-29 00:46:37 +0900

    overall update

--- tx90_moveit_client/scripts/client_tutorial.py
+++ tx90_moveit_client/scripts/client_tutorial.py
@@ -125,9 +125,9 @@ class StaubliScanning(object):
 		## We can plan a motion for this group to a desired pose for the
 		## end-effector:
 		pose_goal = geometry_msgs.msg.Pose()
-		pose_goal.position.x = 0.690	
-		pose_goal.position.y = -0.025
-		pose_goal.position.z = 0.175 #0.73 base
+		pose_goal.position.x = 0.756	
+		pose_goal.position.y = -0.026
+		pose_goal.position.z = 0.273 #0.73 base
 		pose_goal.orientation.x = -0.000
 		pose_goal.orientation.y = 0.707
 		pose_goal.orientation.z = 0.000
@@ -199,5 +199,5 @@ if __name__ == '__main__':
 	# cartesian_plan, fraction = staubli_client.plan_cartesian_path()
 	# staubli_client.execute_plan(cartesian_plan)
 
-	staubli_client.go_to_joint_state()
-	# staubli_client.go_to_pose_goal()
+	#staubli_client.go_to_joint_state()
+	staubli_client.go_to_pose_goal()
