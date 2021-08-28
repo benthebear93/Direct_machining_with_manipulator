d3d8fed57a5ec9634f8e1803d2c3dcf65efc37b5
benthebear93 <nswve@naver.com>
2021-08-29 00:46:03 +0900

    gen surface add profile_sum

--- keyence_ros/src/gen_surface.cpp
+++ keyence_ros/src/gen_surface.cpp
@@ -1,6 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
-#include "std_msgs/Float64MultiArray.h"
+#include "std_msgs/Float32MultiArray.h"
 #include <iostream>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
@@ -27,14 +27,16 @@ class Scanner{
         float pos_x;
         float pos_y;
         float pos_z;
-        int temp;
+        float temp;
         
         std::string frame_id;
         std::string tf_frame;
-        std_msgs::Float64MultiArray arr_profile;
+        //std_msgs::Float64MultiArray arr_profile;
+        std_msgs::Float32MultiArray profile_sum;
         Cloud::Ptr  point3d{new Cloud};
         Cloud::Ptr  sum_pointcloud{new Cloud};
         Cloud::Ptr  temp_pointcloud{new Cloud};
+        pcl::PointCloud<pcl::PointXYZ> sum_point;
         tf::TransformListener listener;
         tf::StampedTransform btolj; //base to LJ sensor
         //vector<xyz> point_storage;
@@ -44,9 +46,10 @@ class Scanner{
         ros::NodeHandle nh_, pnh_{"~"};
 
         ros::Publisher  point_pub_;
-        ros::Publisher  arr_profile_;
-
+        //ros::Publisher  arr_profile_;
+        ros::Publisher  pub_test_;
         ros::Subscriber cloud_sub_;
+        ros::Publisher profile_sum_;
         void cloudMsgCallback(const sensor_msgs::PointCloud2& msg);
 };
 
@@ -64,19 +67,24 @@ Scanner::~Scanner() // destroy
 
 bool Scanner::init()
 {
+    ROS_INFO("init!");
     point_pub_    = nh_.advertise<sensor_msgs::PointCloud2>("CMM_profile", 100); 
-    arr_profile_  = nh_.advertise<std_msgs::Float64MultiArray>("arr_profile", 100);
+    //arr_profile_  = nh_.advertise<std_msgs::Float64MultiArray>("arr_profile", 100);
     cloud_sub_    = nh_.subscribe("/profiles", 1, &Scanner::cloudMsgCallback, this);
+    pub_test_     = nh_.advertise<std_msgs::Float32>("test_topic", 100); 
+    profile_sum_  = nh_.advertise<std_msgs::Float32MultiArray>("profile_sum", 100);
+
     pos_y    = 0;
     pos_x    = 0;
     pos_z    = 0;
-    temp   = 0;
+    temp     = 0;
 
     point3d.reset(new Cloud);
     point3d->header.frame_id  = "tool0";
     point3d->is_dense         = false;
     point3d->width            = 800;
     point3d->height           = 1;
+
     sum_pointcloud.reset(new Cloud);
     temp_pointcloud.reset(new Cloud);
 
@@ -85,13 +93,10 @@ bool Scanner::init()
 
 void Scanner::cloudMsgCallback(const sensor_msgs::PointCloud2& msg)
 {
-    clock_t start, finish;
-    double duration;
- 
     try{
         ros::Time now = ros::Time::now();
-        listener.waitForTransform("base_link", "tool0", now, ros::Duration(0.01));
-        listener.lookupTransform("base_link", "tool0", ros::Time(0), btolj);
+        listener.waitForTransform("base_link", "lj_v7080_optical_frame", now, ros::Duration(0.0001));
+        listener.lookupTransform("base_link", "lj_v7080_optical_frame", ros::Time(0), btolj);
         pos_x = btolj.getOrigin().x();
         pos_y = btolj.getOrigin().y();
         pos_z = btolj.getOrigin().z(); //sensor coordinate from base_link
@@ -99,7 +104,8 @@ void Scanner::cloudMsgCallback(const sensor_msgs::PointCloud2& msg)
     catch (tf::TransformException ex){
         ROS_ERROR("%s",ex.what()); 
         ros::Duration(0.01).sleep();
-    } //0.0006sec
+    }
+    //ROS_INFO("tf time ");
     pcl::PCLPointCloud2 pcl_pc;  // pcl point cloud
     pcl_conversions::toPCL(msg, pcl_pc);  // sensor msg to pcl
 
@@ -107,37 +113,30 @@ void Scanner::cloudMsgCallback(const sensor_msgs::PointCloud2& msg)
     pcl::fromPCLPointCloud2(pcl_pc, input_cloud); // pcl to Pointcloud 
 
     int profile_size = msg.width;
-    point3d -> points.clear();
-    point3d -> points.reserve(profile_size);
-
-    point3d->header.frame_id = "base_link";
-    point3d->is_dense = false; // cloud could have NaNs
-    point3d->height = 1;
     float x = 0,y = 0,z =0; //5e-05sec
-    start = clock();
-    for(int i =0; i<profile_size; i++)
+    for(int i =0; i<profile_size; i+=8)
     {
         x = input_cloud.points[i].x + pos_x;
         y = input_cloud.points[i].y + pos_y;
         z = input_cloud.points[i].z + pos_z;
-        if(z > 10 or x >10 or y>10) // inf
+        if(z>10) // inf
         {
-            z = 1;
-            x = 1;
+            z = 10;
+            y = 10;
         }
-        point3d->points.push_back(pcl::PointXYZ(x, y, z)); //push back points
-        //int check_y = int(y*100000);
-        if(pos_y!=temp)
-        {   
-            if(i==profile_size-1)
-            {
-                *sum_pointcloud = *sum_pointcloud + *point3d; 
-            }
-        } 
-        // else
-    }//3e-05sec
-    temp = pos_y;
-    // ROS_INFO("check");
+        profile_sum.data.push_back(x);
+        profile_sum.data.push_back(y);
+        profile_sum.data.push_back(z);
+        if(i==profile_size-8)
+        {
+            //ROS_INFO("pubping");
+            profile_sum_.publish(profile_sum);
+            profile_sum.data.clear();
+        }
+    }
+    std_msgs::Float32 test;
+    test.data = pos_x;
+    pub_test_.publish(test);
 }
 
 int main(int argc, char**argv)
@@ -145,9 +144,9 @@ int main(int argc, char**argv)
     ros::init(argc, argv, "generate_surface");
     Scanner Scanner;
     ros::spin();
-    if (!ros::ok()) {
-        ROS_INFO("save pcd");
-        std::string fileName = Scanner.filepath +"/pcd_data/profile.pcd";
-        pcl::io::savePCDFileASCII(fileName, *Scanner.sum_pointcloud);
-    }
+    // if (!ros::ok()) {
+    //     ROS_INFO("save pcd");
+    //     std::string fileName = Scanner.filepath +"/pcd_data/profile_hz.pcd";
+    //     pcl::io::savePCDFileASCII(fileName, *Scanner.sum_pointcloud);
+    // }
 }
