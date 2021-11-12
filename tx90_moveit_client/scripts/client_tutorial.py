547f011fc5daf872f5ec65da3d7f0155cea92fcd
benthebear93 <nswve@naver.com>
2021-11-13 01:07:23 +0900

    calibration added

--- tx90_moveit_client/scripts/client_tutorial.py
+++ tx90_moveit_client/scripts/client_tutorial.py
@@ -91,24 +91,24 @@ class StaubliScanning(object):
 	def go_to_joint_state(self):
 		move_group = self.move_group
 
-		joint_goal = move_group.get_current_joint_values()
-		joint_goal[0] = 0
-		joint_goal[1] = np.deg2rad(0)
-		joint_goal[2] = np.deg2rad(90)
-		joint_goal[3] = 0
-		joint_goal[4] = np.deg2rad(0)
-		joint_goal[5] = np.deg2rad(0)  # 1/6 of a turn
+		# joint_goal = move_group.get_current_joint_values()
+		# joint_goal[0] = 0
+		# joint_goal[1] = np.deg2rad(0)
+		# joint_goal[2] = np.deg2rad(90)
+		# joint_goal[3] = 0
+		# joint_goal[4] = np.deg2rad(0)
+		# joint_goal[5] = np.deg2rad(0)  # 1/6 of a turn
 
-		# The go command can be called with joint values, poses, or without any
-		# parameters if you have already set the pose or joint target for the group
-		move_group.go(joint_goal, wait=True)
+		# # The go command can be called with joint values, poses, or without any
+		# # parameters if you have already set the pose or joint target for the group
+		# move_group.go(joint_goal, wait=True)
 
-		# Calling ``stop()`` ensures that there is no residual movement
-		move_group.stop()
+		# # Calling ``stop()`` ensures that there is no residual movement
+		# move_group.stop()
 
-		## END_SUB_TUTORIAL
+		# ## END_SUB_TUTORIAL
 
-		# For testing:
+		# # For testing:
 		current_joints = move_group.get_current_joint_values()
 		print("                              ")
 		print("current joint ",current_joints)
@@ -120,14 +120,14 @@ class StaubliScanning(object):
 
 
 		pose_goal = geometry_msgs.msg.Pose()
-		pose_goal.position.x = 0.724-0.002 # 3mm offset 
-		pose_goal.position.y = -0.055+0.0045 # 5mm offset
-		pose_goal.position.z = -0.260 + 0.478 # negative is actual z position value 
+		pose_goal.position.x = 0.7621 - 0.0027 # 3mm offset 
+		pose_goal.position.y = 0.008 + 0.0038 # 5mm offset
+		pose_goal.position.z = 0.1649 - 0.5591 + 0.478 # negative is actual z position value 
 
-		pose_goal.orientation.x = -0.00232593
-		pose_goal.orientation.y = -0.93067
-		pose_goal.orientation.z = 0.00240073                                                                                             
-		pose_goal.orientation.w = -0.365844	
+		pose_goal.orientation.x = 0.00027774
+		pose_goal.orientation.y = 0.707682
+		pose_goal.orientation.z = 0.0003458                                                                                             
+		pose_goal.orientation.w = 0.706530	
 
 		move_group.set_pose_target(pose_goal)
 
@@ -168,6 +168,6 @@ if __name__ == '__main__':
 	
 	# cartesian_plan, fraction = staubli_client.plan_cartesian_path()
 	# staubli_client.execute_plan(cartesian_plan)
-
+	# staubli_client.find_curr_pose()
 	# staubli_client.go_to_joint_state()
 	staubli_client.go_to_pose_goal()
